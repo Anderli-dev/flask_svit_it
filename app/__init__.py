d57309974f9e68bedf2b74418f7259568061ceb7
Ivan Suprun <60109954+Anderli-dev@users.noreply.github.com>
2025-03-11 09:00:51 +0200

    revorked creation and run the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,15 @@
 from flask import Flask
-from flask_alembic import Alembic
-from flask_sqlalchemy import SQLAlchemy
 
-from app.models.base import Base
+from app.utils.db import db
 from config.config import Config
 from app.api.routes import api_bp
 
-app = Flask(__name__)
-app.config.from_object(Config())
-
-db = SQLAlchemy(model_class=Base)
-
-alembic = Alembic(metadatas=Base.metadata)
-
-db.init_app(app)
-alembic.init_app(app)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config())
     
-app.register_blueprint(api_bp, url_prefix="/api")
-    
+    db.init_app(app)
+
+    app.register_blueprint(api_bp, url_prefix="/api")
+
+    return app
