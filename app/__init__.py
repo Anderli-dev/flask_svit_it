3eca29a37388047e7927b2e66888bdc9eafe6930
Ivan Suprun <60109954+Anderli-dev@users.noreply.github.com>
2025-03-11 07:00:30 +0200

    added alembic

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask_alembic import Alembic
+from flask_sqlalchemy import SQLAlchemy
 
+from app.models.base import Base
 from config.config import Config
 from app.api.routes import api_bp
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config.from_object(Config())
 
+db = SQLAlchemy(model_class=Base)
+
+alembic = Alembic(metadatas=Base.metadata)
+
+db.init_app(app)
+alembic.init_app(app)
+    
 app.register_blueprint(api_bp, url_prefix="/api")
     
--- config/config.py
+++ config/config.py
@@ -1,8 +1,8 @@
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
-
 
 class Config:
     APP_PORT=os.getenv('APP_PORT')
@@ -12,5 +12,11 @@ class Config:
     POSTGRES_PASSWORD=os.getenv('POSTGRES_PASSWORD')
     POSTGRES_DB=os.getenv('POSTGRES_DB')
     POSTGRES_PORT=os.getenv('POSTGRES_PORT')
+    POSTGRES_HOST=os.getenv('POSTGRES_HOST')
     
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    @property
+    def SQLALCHEMY_DATABASE_URI(self) -> str:
+        return f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"  
     
